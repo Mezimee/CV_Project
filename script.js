35f3231e2adeb9972452cd44bae3f044ae99b363
Michal <michalmaciejewskioffice@gmail.com>
2024-12-05 01:01:15 +0100

    Code expansion in index.html, style.css and script.js. Add some cool hover effects.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuToggle = document.getElementById("menu-toggle");
 const sideMenu = document.getElementById("side-menu");
 
+// Efekt otwierania menu
 menuToggle.addEventListener("click", () => {
     sideMenu.classList.toggle("open");
 })
